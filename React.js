b1affe14c157831b47f8dca8301e2f4dc91968e5
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-12-20 12:32:13 +0530

    Update React.js

--- React.js
+++ React.js
@@ -311,6 +311,132 @@ index.js
 =======
 // just app component import
 
+ contact.js
+============
+const contacts = [
+  {
+    name: "",
+    imgURL:
+      "",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
+
+components/app.js
+====================
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
+
+component/cart.js
+=================
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
+
+// --------------------------------------------------------  React Conditional Rendering with the Ternary Operator & AND Operator
+
+app.js
+=============
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(2023, 11, 20, 13).getHours();
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/*AND Operator*/}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
+}
+
+export default App;
+
+Login.js
+===========
+import React from "react";
+import Input from "./Input";
+
+function Login() {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
+
+
 // --------------------------------------------------------  React props
 
 
