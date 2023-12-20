c48e0168f88037a3676d815db9ea564cd0c0f365
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-12-20 10:08:56 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,3 +1,8 @@
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root));
+root.render(<App />);
+---------------------------------------------------------------------------
+
 import React from "react";
 import ReactDOM from "react-dom";
 
