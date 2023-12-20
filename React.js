fa9e96fe97b6cf89a91e13eab39ddfceb1b338fa
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2023-12-20 11:23:33 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
-const root = ReactDOM.createRoot(document.getElementById("root));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
----------------------------------------------------------------------------
+// ---------------------------------------------  ------Introduction to JSX
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -14,7 +14,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
------------------------------------------------------
+// ----------------------------------------------------- JSX Code Practice
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -29,3 +29,312 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+// ------------------------------------------------------- Javascript Expressions in JSX & ES6 Template Literals
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Marcello";
+const lName = "Tech";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// -------------------------------------------------------- Javascript Expressions in JSX Practice
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Marcello";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+
+// -------------------------------------------------------- JSX Attributes & Styling React Elements
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"} />
+
+    <img
+      className="food-img"
+      alt=""
+      src=""
+    />
+    <img
+      className="food-img"
+      alt=""
+      src=""
+    />
+    <img
+      className="food-img"
+      alt=""
+      src=""
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+
+// -------------------------------------------------------- Inline Styling for React Elements
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
+
+
+// -------------------------------------------------------- React Styling Practice
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
+
+// -------------------------------------------------------- React Components
+index.js
+=========
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+app.js
+=========
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+      <List />
+    </div>
+  );
+}
+
+export default App;
+
+heading.js
+===========
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;
+
+list.js
+========
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
+
+
+// -------------------------------------------------------- import and export
+index.js
+=========
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+math.js
+=======
+const pi = 3.14;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { doublePi, triplePi };
+
+// -------------------------------------------------------- import and export practice
+
+index.js
+===========
+import React from "react";
+import ReactDOM from "react-dom";
+import { add, subtract, multiply, divide } from "./calculator";
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+calculator.js
+===============
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+export { add, multiply, subtract, divide };
+
+// --------------------------------------------------------  React props
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name=""
+      img=""
+      tel=""
+      email=""
+    />
+    <Card
+      name=""
+      img=""
+      tel=""
+      email=""
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+// --------------------------------------------------------  React props practice
+
+index.js
+=======
+// just app component import
+
+// --------------------------------------------------------  React props
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
